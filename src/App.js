09cb553232bb5c9a9cc54b50f4f2d84fafc59162
slartibastfast <ciaran.finnegan@gmail.com>
2020-09-17 23:18:06 +0800

    linting fixes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import { LinkContainer } from "react-router-bootstrap";
+// import { LinkContainer } from "react-router-bootstrap";
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import { Nav, Navbar, NavItem } from "react-bootstrap";
+// import { Link } from "react-router-dom";
+// import { Nav, Navbar, NavItem } from "react-bootstrap";
 import "./App.css";
 import Routes from "./Routes";
 import { AppContext } from "./libs/contextLib";
 import { Auth } from "aws-amplify";
-import { useHistory } from "react-router-dom";
+// import { useHistory } from "react-router-dom";
 import { onError } from "./libs/errorLib";
 import ErrorBoundary from "./components/ErrorBoundary";
 
 function App() {
-  const history = useHistory();
+  //const history = useHistory();
   const [isAuthenticated, userHasAuthenticated] = useState(false);
   const [isAuthenticating, setIsAuthenticating] = useState(true);
 
@@ -32,15 +32,16 @@ function App() {
     setIsAuthenticating(false);
   }
 
-  async function handleLogout() {
-    await Auth.signOut();
+  // async function handleLogout() {
+  //   await Auth.signOut();
 
-    userHasAuthenticated(false);
+  //   userHasAuthenticated(false);
 
-    history.push("/login");
-  }
+  //   history.push("/login");
+  // }
 
   return (
+    !isAuthenticating && (
     <div className="app-frame">
       <header>
         <h1>Shhh</h1>
@@ -62,6 +63,7 @@ function App() {
         </p>
       </footer>
     </div>
+    )
   );
 
   // return (
