017cdbd19fdc0b765cd6ce7b99fe9966b0ce557f
slartibastfast <ciaran.finnegan@gmail.com>
2021-04-07 13:58:30 +0800

    bugfix - linting error

--- src/containers/Team.js
+++ src/containers/Team.js
@@ -1,5 +1,5 @@
 import React from "react";
-import PropTypes from "prop-types";
+// import PropTypes from "prop-types";
 import { Button } from "react-bootstrap";
 import API from "@aws-amplify/api";
 import InviteTeamMemberModal from "../components/InviteTeamMemberModal";
